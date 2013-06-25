c964157b3627b8a0709292768fd718b497664caa
spartam <kwpardon@infogroep.be>
2013-06-26 00:21:36 +0200

    initial

---
+++ InfoManager/InfoManager/src/be/infogroep/InfoManager/InfoManager.java
@@ -0,0 +1,14 @@
+package be.infogroep.InfoManager;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class InfoManager extends JavaPlugin{
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		
+		return true;
+	}
+	
+}
